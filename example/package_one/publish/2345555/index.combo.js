e5124e48b1f1d05e6dd4d6f5c08579fff0d8aea1
neekey <ni184775761@gmail.com>
2012-08-18 15:37:51 +0800

    update

---
+++ example/package_one/publish/2345555/index.combo.js
@@ -0,0 +1,13 @@
+/*
+combined files : 
+
+package_one/mod
+package_one/index
+
+*/
+KISSY.add('package_one/mod',function(){
+    console.log( 'mod' );
+});KISSY.add('package_one/index',function(){
+
+    console.log( 'index');
+},{requires: [ './mod' ]});
