145bda3215a6b1d4d763eb2c99afaf1491380db7
neekey <ni184775761@gmail.com>
2012-08-19 23:00:24 +0800

    update

---
+++ test/getPackagePath.spec.js
@@ -0,0 +1,55 @@
+var util = require( '../lib/util' );
+
+describe( 'Test for method getPackagePath', function(){
+
+    it('Normal', function(){
+        var packageName = 'pkgName';
+        var currentPath = '/users/neekey/Dropbox/pkgName/to/current/path';
+        var expectPath = '/users/neekey/Dropbox';
+
+        var actualPath = util.getPackagePath( packageName, currentPath );
+        expect( expectPath ).toEqual( actualPath );
+    });
+
+    it('Relative', function(){
+        var packageName = 'pkgName';
+        var currentPath = '../neekey/Dropbox/pkgName/to/current/path';
+        var expectPath = '../neekey/Dropbox';
+
+        var actualPath = util.getPackagePath( packageName, currentPath );
+        expect( expectPath ).toEqual( actualPath );
+
+        packageName = 'pkgName';
+        currentPath = 'neekey/Dropbox/pkgName/to/current/path';
+        expectPath = 'neekey/Dropbox';
+
+        actualPath = util.getPackagePath( packageName, currentPath );
+        expect( expectPath ).toEqual( actualPath );
+
+        packageName = 'pkgName';
+        currentPath = './neekey/Dropbox/pkgName/to/current/path';
+        expectPath = 'neekey/Dropbox';
+
+        actualPath = util.getPackagePath( packageName, currentPath );
+        expect( expectPath ).toEqual( actualPath );
+    });
+
+    it('Not exists', function(){
+
+        var packageName = 'pkgName';
+        var currentPath = 'neekey/Dropbox/to/current/path';
+        var expectPath = undefined;
+
+        var actualPath = util.getPackagePath( packageName, currentPath );
+        expect( expectPath ).toEqual( actualPath );
+    });
+
+    it('First', function(){
+        var packageName = 'pkgName';
+        var currentPath = 'pkgName/neekey/Dropbox/to/current/path';
+        var expectPath = undefined;
+
+        var actualPath = util.getPackagePath( packageName, currentPath );
+        expect( expectPath ).toEqual( actualPath );
+    })
+});
